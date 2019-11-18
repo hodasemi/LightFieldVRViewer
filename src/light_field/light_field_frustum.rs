f17938ba829619ec049708bb9234dc723e6c840b
hodasemi <michaelh.95@t-online.de>
2019-11-18 20:20:18 +0100

    Use fstop correctly

--- src/light_field/light_field_frustum.rs
+++ src/light_field/light_field_frustum.rs
@@ -54,7 +54,7 @@ impl LightFieldFrustum {
                 let current_center =
                     total_top_left + (x as f32 * baseline * right) - (y as f32 * baseline * up);
 
-                frustums.push(Self::new(
+                frustums.push(Self::as_f_stop(
                     x,
                     y,
                     current_center,
@@ -69,27 +69,56 @@ impl LightFieldFrustum {
         frustums
     }
 
-    fn new(
+    fn as_aperture_size(
         x: usize,
         y: usize,
-        center: Vector3<f32>,
+        camera_center: Vector3<f32>,
         direction: Vector3<f32>,
         up: Vector3<f32>,
         right: Vector3<f32>,
         config: &Config,
     ) -> Self {
-        let sensor_center = center - direction * config.intrinsics.focal_length();
+        let sensor_center = camera_center - direction * config.intrinsics.focal_length();
 
         let (sensor_left_top, sensor_left_bottom, sensor_right_top, sensor_right_bottom) =
             Self::calculate_corners(sensor_center, up, right, config.intrinsics.sensor_size());
 
         let (aperture_left_top, aperture_left_bottom, aperture_right_top, aperture_right_bottom) =
-            Self::calculate_corners(center, up, right, config.intrinsics.fstop());
+            Self::calculate_corners(camera_center, up, right, config.intrinsics.fstop());
 
         let left_top = Line::create(aperture_left_top, sensor_left_top);
         let left_bottom = Line::create(aperture_left_bottom, sensor_left_bottom);
         let right_top = Line::create(aperture_right_top, sensor_right_top);
         let right_bottom = Line::create(aperture_right_bottom, sensor_right_bottom);
+
+        LightFieldFrustum {
+            position: (x, y),
+
+            left_top,
+            left_bottom,
+            right_top,
+            right_bottom,
+        }
+    }
+
+    fn as_f_stop(
+        x: usize,
+        y: usize,
+        camera_center: Vector3<f32>,
+        direction: Vector3<f32>,
+        up: Vector3<f32>,
+        right: Vector3<f32>,
+        config: &Config,
+    ) -> Self {
+        let sensor_center = camera_center - direction * config.intrinsics.focal_length();
+
+        let (sensor_left_top, sensor_left_bottom, sensor_right_top, sensor_right_bottom) =
+            Self::calculate_corners(sensor_center, up, right, config.intrinsics.sensor_size());
+
+        let left_top = Line::create(camera_center, sensor_right_bottom);
+        let left_bottom = Line::create(camera_center, sensor_right_top);
+        let right_top = Line::create(camera_center, sensor_left_bottom);
+        let right_bottom = Line::create(camera_center, sensor_left_top);
 
         LightFieldFrustum {
             position: (x, y),
